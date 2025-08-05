b13fbdd4ba6302897f759c9005e8c1c5f7ebc089
Muhammed-Mudassir <Muhammed@synergyresourcesgrp.com>
2025-08-06 00:29:20 +0530

    Added synergy-demo-frontend as a regular fold

---
+++ synergy-demo-frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "synergyfrontend673970db": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "Synergy2025": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
