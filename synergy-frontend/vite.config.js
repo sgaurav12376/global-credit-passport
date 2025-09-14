0ef968e2d1eb8317d434a21e71a49a08c34ea6b3
bharath-709457 <79187084+bharath-709457@users.noreply.github.com>
2025-09-14 09:52:45 -0500

    Global Credit App | Updating backend URL

--- synergy-frontend/vite.config.js
+++ synergy-frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "https://xueqaf59oh.execute-api.us-east-1.amazonaws.com",
         changeOrigin: true,
       },
     },
