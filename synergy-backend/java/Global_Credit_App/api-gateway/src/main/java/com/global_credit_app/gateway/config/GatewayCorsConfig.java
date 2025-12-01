a1c35bc3155f251bada4437d4d3f26f650add7be
bharath-709457 <79187084+bharath-709457@users.noreply.github.com>
2025-11-30 21:43:44 -0600

    Global_Credit_App | Updating New AWS Frontend URL in Backend code (Cross Origin)

--- synergy-backend/java/Global_Credit_App/api-gateway/src/main/java/com/global_credit_app/gateway/config/GatewayCorsConfig.java
+++ synergy-backend/java/Global_Credit_App/api-gateway/src/main/java/com/global_credit_app/gateway/config/GatewayCorsConfig.java
@@ -14,7 +14,7 @@ public class GatewayCorsConfig {
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173","http://localhost:3000","https://main.d1op9zp2fj4k1k.amplifyapp.com"));
+        config.setAllowedOrigins(List.of("http://localhost:5173","http://localhost:3000","https://main.d1op9zp2fj4k1k.amplifyapp.com","https://main.d1et75e6p8hli6.amplifyapp.com"));
         config.setAllowCredentials(true);
         config.setAllowedHeaders(List.of("*"));
         config.setAllowedMethods(List.of("*"));
