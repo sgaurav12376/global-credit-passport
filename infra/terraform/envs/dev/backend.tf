e36e982814cbdbcd43e5f711d079cfa1bca06203
Gaurav Chug <sgaurav12376@gmail.comm>
2026-02-03 03:08:39 +0530

    Fix Terraform dev env: backend region, variables syntax, versions file, cleanup

--- infra/terraform/envs/dev/backend.tf
+++ infra/terraform/envs/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "synergy-terraform-state-gcp"
+    bucket         = "synergy-terraform-state-gcp-use1"
     key            = "gcp/dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "synergy-terraform-locks"
--- infra/terraform/envs/dev/main.tf
+++ infra/terraform/envs/dev/main.tf
@@ -17,37 +17,37 @@ module "ecr" {
 }
 
 module "compute_java" {
-  source              = "../../modules/compute_service"
-  project             = var.project
-  service_name        = "java-api"
-  vpc_id              = module.network.vpc_id
-  private_subnet_ids  = module.network.private_subnet_ids
-  instance_sg_id      = module.security.ec2_sg_id
-  alb_sg_id           = module.security.alb_sg_id
-  container_port      = var.java_container_port
-  ecr_repo_url        = module.ecr.java_repo_url
-  image_tag           = var.java_image_tag
+  source             = "../../modules/compute_service"
+  project            = var.project
+  service_name       = "java-api"
+  vpc_id             = module.network.vpc_id
+  private_subnet_ids = module.network.private_subnet_ids
+  instance_sg_id     = module.security.ec2_sg_id
+  alb_sg_id          = module.security.alb_sg_id
+  container_port     = var.java_container_port
+  ecr_repo_url       = module.ecr.java_repo_url
+  image_tag          = var.java_image_tag
 }
 
 module "compute_python" {
-  source              = "../../modules/compute_service"
-  project             = var.project
-  service_name        = "python-score"
-  vpc_id              = module.network.vpc_id
-  private_subnet_ids  = module.network.private_subnet_ids
-  instance_sg_id      = module.security.ec2_sg_id
-  alb_sg_id           = module.security.alb_sg_id
-  container_port      = var.python_container_port
-  ecr_repo_url        = module.ecr.python_repo_url
-  image_tag           = var.python_image_tag
+  source             = "../../modules/compute_service"
+  project            = var.project
+  service_name       = "python-score"
+  vpc_id             = module.network.vpc_id
+  private_subnet_ids = module.network.private_subnet_ids
+  instance_sg_id     = module.security.ec2_sg_id
+  alb_sg_id          = module.security.alb_sg_id
+  container_port     = var.python_container_port
+  ecr_repo_url       = module.ecr.python_repo_url
+  image_tag          = var.python_image_tag
 }
 
 module "alb" {
-  source              = "../../modules/alb"
-  project             = var.project
-  vpc_id              = module.network.vpc_id
-  private_subnet_ids  = module.network.private_subnet_ids
-  alb_sg_id           = module.security.alb_sg_id
+  source             = "../../modules/alb"
+  project            = var.project
+  vpc_id             = module.network.vpc_id
+  private_subnet_ids = module.network.private_subnet_ids
+  alb_sg_id          = module.security.alb_sg_id
 
   java_target_group_arn   = module.compute_java.target_group_arn
   python_target_group_arn = module.compute_python.target_group_arn
@@ -68,8 +68,8 @@ module "apigw" {
   private_subnet_ids = module.network.private_subnet_ids
   vpclink_sg_id      = module.security.vpclink_sg_id
 
-  alb_listener_arn   = module.alb.listener_arn
+  alb_listener_arn = module.alb.listener_arn
 
-  cognito_issuer     = module.cognito.issuer
-  cognito_audience   = module.cognito.app_client_id
+  cognito_issuer   = module.cognito.issuer
+  cognito_audience = module.cognito.app_client_id
 }
